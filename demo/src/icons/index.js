4d7045d8ae9424918169428bba90506106fa0c18
David Edelhart <dave@wonderlandlabs.com>
2020-10-27 10:59:38 -0700

    v1.0.19

--- demo/src/icons/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CheckOff } from './CheckOff'
-export { default as CheckOn } from './CheckOn'
-export { default as RadioOff } from './RadioOff'
-export { default as RadioOn } from './RadioOn'
