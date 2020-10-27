e90dcba0fbc4eeb272aafefe476d4804f0720799
David Edelhart <dave@wonderlandlabs.com>
2020-10-27 00:47:21 -0700

    refactored events; being noisy for now

--- src/listenForPointers.js
+++ src/listenForPointers.js
@@ -2,56 +2,65 @@ const moveThreshold = 5;
 const maxDuration = 4 * 1000;
 
 export default (state) => {
+  if (!document && document.addEventListener) {
+    console.log('cannot listen for events - no document');
+    return;
+  }
+  console.log('listening for pointer events: ', state);
+
   const pointers = new Map();
   state.do.setListeningForPointers(true);
 
-  state.action('pointerDown', (state, event) => {
+  const onPointerDown = (event) => {
     const { pointerId, clientX, clientY } = event;
-    pointers.set(pointerId, { clientX, clientY, at: Date.now() });
-    setTimeout(() => pointers.delete(clientX), maxDuration);
-  });
+    const newPointer = {
+      clientX, clientY, pointerId, at: Date.now(),
+    };
+    pointers.set(pointerId, newPointer);
+    console.log('created', newPointer);
 
-  state.action('pointerMove', (state, event) => {
+    setTimeout(() => pointers.delete(clientX), maxDuration);
+  };
+
+  const onPointerMove = (event) => {
     const { pointerId, clientX, clientY } = event;
     const storedPointer = pointers.get(pointerId);
     if ((!storedPointer)
-        || (storedPointer.dragging)) {
+      || (storedPointer.dragging)) {
       return;
     }
 
     const dx = Math.abs(clientX - storedPointer.clientX);
     const dy = Math.abs(clientY - storedPointer.clientY);
+    console.log('moved ', dx, dy, 'for pointer ', storedPointer);
 
     if (Math.max(dx, dy) > moveThreshold) {
       storedPointer.dragging = true;
     }
-  });
+  };
 
-  state.action('pointerEnd', (state, event) => {
+  const onPointerEnd = (state, event) => {
     const { pointerId } = event;
 
     const storedPointer = pointers.get(pointerId);
     if (storedPointer) {
       if (!storedPointer.dragging) {
+        console.log('closing because of pointer end: ', storedPointer);
         state.do.setDisplay(false);
       }
       pointers.delete(pointerId);
     }
-  });
+  };
 
-  if (document && document.addEventListener) {
-    document.addEventListener('pointerDown', state.do.pointerDown, false);
-    document.addEventListener('pointermove', state.do.pointerMove, false);
-    document.addEventListener('pointerend', state.do.pointerEnd, false);
-  }
+  document.addEventListener('pointerDown', onPointerDown, false);
+  document.addEventListener('pointermove', onPointerMove, false);
+  document.addEventListener('pointerend', onPointerEnd, false);
 
   // cancel listeners on state completion
   state.subscribe(() => {}, () => {},
     () => {
-      if (document && document.removeEventListener) {
-        document.removeEventListener('pointerDown', state.do.pointerDown);
-        document.removeEventListener('pointermove', state.do.pointerMove);
-        document.removeEventListener('pointerend', state.do.pointerEnd);
-      }
+      document.removeEventListener('pointerDown', onPointerDown);
+      document.removeEventListener('pointermove', onPointerMove);
+      document.removeEventListener('pointerend', onPointerEnd);
     });
 };
