72caae583f7a9ab78299c7de779279c6314f7ca2
Edelhart.Dave <dave.edelhart@mckesson.com>
2021-02-23 10:08:48 -0800

    synchronized latest version

--- src/storeFactory.js
+++ src/storeFactory.js
@@ -1,4 +1,4 @@
-import { ValueStoreObject, ValueStream } from '@wonderlandlabs/looking-glass-engine';
+import { ValueObjectStream, addActions, ValueStream } from '@wonderlandlabs/looking-glass-engine';
 import isEqual from 'lodash.isequal';
 
 const defaultOptionToLabel = (option) => {
@@ -30,7 +30,7 @@ export default (props = {}) => {
   const options = props.options || [];
   const choices = props.choices || [];
 
-  const state = new ValueStoreObject({
+  const state = addActions(new ValueObjectStream({
     options, // entire population
     choices, // chosen items
     display: !!props.display,
@@ -41,65 +41,63 @@ export default (props = {}) => {
     optionToLabel,
     optionDisabled,
     closeOnClick: !!props.closeOnClick,
-  }, {
-    actions: {
-      /**
+  }), {
+    /**
        * adds (or removes) a choice into the choices based on an option
        *  note - there is no filtering/consequence for adding an option that
        * is not in the options.
        */
-      chooseOption(store, option) {
-        if (store.my.chooseOne) {
-          const choice = store.my.optionToChoice(option);
-          store.do.setChoices([choice]);
-          return;
-        }
-        store.do.toggleOption(option);
-      },
-      /**
+    chooseOption(store, option) {
+      if (store.my.chooseOne) {
+        const choice = store.my.optionToChoice(option);
+        store.do.setChoices([choice]);
+        return;
+      }
+      store.do.toggleOption(option);
+    },
+    /**
        * adds an option - but unlike chooseOption, doesn't remove it if its already present
        */
-      addOption(store, option) {
-        const newChoice = store.my.optionToChoice(option);
+    addOption(store, option) {
+      const newChoice = store.my.optionToChoice(option);
+      if (store.my.chooseOne) {
+        store.do.setChoices([newChoice]);
+        return;
+      }
+      const isActive = store.my.choices.some((choice) => store.my.comparator(choice, newChoice));
+      if (!isActive) {
+        store.do.setChoices([...store.my.choices, newChoice]);
+      }
+    },
+    toggleOption(store, option) {
+      const newChoice = store.my.optionToChoice(option);
+      const isActive = store.my.choices.some((choice) => store.my.comparator(choice, newChoice));
+      if (isActive) {
         if (store.my.chooseOne) {
-          store.do.setChoices([newChoice]);
-          return;
-        }
-        const isActive = store.my.choices.some((choice) => store.my.comparator(choice, newChoice));
-        if (!isActive) {
-          store.do.setChoices([...store.my.choices, newChoice]);
-        }
-      },
-      toggleOption(store, option) {
-        const newChoice = store.my.optionToChoice(option);
-        const isActive = store.my.choices.some((choice) => store.my.comparator(choice, newChoice));
-        if (isActive) {
-          if (store.my.chooseOne) {
-            store.do.setChoices([]);
-          } else {
-            // remove choices that equate to how we would store the option
-            store.do.setChoices(store.my.choices.filter((choice) => !store.my.comparator(choice, newChoice)));
-          }
-        } else if (store.my.chooseOne) {
-          store.do.setChoices([newChoice]);
+          store.do.setChoices([]);
         } else {
-          store.do.setChoices([...store.my.choices, newChoice]);
+          // remove choices that equate to how we would store the option
+          store.do.setChoices(store.my.choices.filter((choice) => !store.my.comparator(choice, newChoice)));
         }
-      },
-      toggleDisplay(store) {
-        store.do.setDisplay(!store.my.display);
-      },
-      addAll(store) {
-        store.do.setChoices([...store.my.options]);
-      },
-      remAll(store) {
-        store.do.setChoices([]);
-      },
+      } else if (store.my.chooseOne) {
+        store.do.setChoices([newChoice]);
+      } else {
+        store.do.setChoices([...store.my.choices, newChoice]);
+      }
+    },
+    toggleDisplay(store) {
+      store.do.setDisplay(!store.my.display);
+    },
+    addAll(store) {
+      store.do.setChoices([...store.my.options]);
+    },
+    remAll(store) {
+      store.do.setChoices([]);
     },
   });
 
-  state.addStream('options', new ValueStream(options))
-    .addStream('choices', new ValueStream(choices));
+  state.addFieldSubject('options', new ValueStream(options));
+  state.addFieldSubject('choices', new ValueStream(choices));
 
   return state;
 };
